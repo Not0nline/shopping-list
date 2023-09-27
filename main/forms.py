77cf7fca709a3707a2b0d031ddaf54adfacbdf36
Adrial Natanael Liong <adrial2_nathanael@yahoo.com>
2023-09-27 14:37:11 +0700

    tutorial 4 selesai

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,14 @@
 from django.forms import ModelForm
 from main.models import Product
+from django import forms
+from django.contrib.auth.models import User 
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ["name", "price", "description"]
+
+    
+    # Baris baru ini menimpa atau 'override' field user yang otomatis ada dari ModelForm.
+    # Gunanya untuk menambahkan atribut tertentu, seperti required=True pada contoh
+    user = forms.ModelChoiceField(queryset=User.objects.all(), required=True)
